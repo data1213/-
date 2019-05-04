6dacea19305249b598f477a927bc8575e559e0f1
data1213 <39784168+data1213@users.noreply.github.com>
2019-05-04 13:46:29 +0800

    第二版
    更新获取计数的方式

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,12 +20,16 @@ MainWindow::~MainWindow()
 
 //计时周期到响应的槽函数
 void MainWindow::update_my_timer(){
-    //1、更新当前计数
-    this->count_time = this->count_time.addMSecs(1); //增加1ms
-    //2、控件显式更新
-    QString timev = this->count_time.toString("hh:mm:ss.zzz");
-    qDebug()<<timev;
-    this->ui->lcdNumber->display(timev);
+    //记录当前时间
+    QTime c_time = QTime::currentTime();
+    int diff = this->count_time.msecsTo(c_time);
+
+    QTime temp;
+    temp.setHMS(0,0,0,0);
+
+    temp = temp.addMSecs(diff);
+    QString str = temp.toString("hh:mm:ss.zzz");
+    this->ui->lcdNumber->display(str);
 
 }
 
@@ -33,6 +37,6 @@ void MainWindow::update_my_timer(){
 void MainWindow::on_btn_begin_clicked()
 {
     this->p_timer->start(1);  //启动1ms周期的定时器开始计时
-    //0、清空计数
-    this->count_time.setHMS(0,0,0,0);
+    //0、记录当前时间
+    this->count_time = QTime::currentTime();
 }
